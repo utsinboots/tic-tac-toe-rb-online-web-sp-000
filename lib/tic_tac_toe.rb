770b1decfdaff0bc3a213fca9b836e9051d3fd38
Utshant Gurung <utshantgurung@gmail.com>
2021-05-10 03:25:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,7 +74,7 @@ def won?(board)
      if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
        return combination
      end
-    end
+   end
   else
     false
   end
