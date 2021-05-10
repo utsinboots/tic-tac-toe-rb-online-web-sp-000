b755dea0c4e79d0a9b8b342c9085d58b14df8070
Utshant Gurung <utshantgurung@gmail.com>
2021-05-10 04:56:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,11 +25,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?(board, index)
-    true
-  else
-    false
-  end
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn_count(board)
