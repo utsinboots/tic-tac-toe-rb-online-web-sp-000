a779ea5755fa9ee4e4c4ae6ea034044bc6907992
Utshant Gurung <utshantgurung@gmail.com>
2021-05-10 03:23:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,7 +61,7 @@ end
 
 def won?(board)
 #WIN_COMBINATIONS.each do |combination|
-if WIN_COMBINATIONS.each do |win_combination|
+  if WIN_COMBINATIONS.each do |win_combination|
     win_index_1 = combination[0]
     win_index_2 = combination[1]
     win_index_3 = combination[2]
@@ -71,10 +71,13 @@ if WIN_COMBINATIONS.each do |win_combination|
     position_3 = board[win_index_3]
     
  #   if position_1 == position_2 && position_2 == position_3
-      return combination
+     if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
+       return combination
+     end
     end
-  end
+  else
     return false
+  end
 end
 
 def full?(board)
